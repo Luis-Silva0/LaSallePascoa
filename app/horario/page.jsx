79bcd1e6196260f83536b22d046b40a82d307bc7
luissilva <biokiller134@gmail.com>
2024-03-02 14:51:38 +0000

    Schedule component

--- app/horario/page.jsx
+++ app/horario/page.jsx
@@ -1,9 +1,17 @@
 "use client";
+import activities from "/data/atividades";
+import Schedule from "./Schedule";
+
+const atividades = activities.Atividades;
 
 export default function Schedule() {
+
     return (
-        <div>
-            Olá
+        <div className="bg-white flex flex-col">
+            <div className="flex pl-[30%] py-[50px] text-[#015450]">
+                Schedule
+            </div>
+            <Schedule />
         </div>
     );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,18 +4,18 @@ import { Link } from "@nextui-org/react";
 
 const Nav = () => {
     return (
-      <div className="bg-white w-full h-[135px] flex flex-col">
+      <div className="bg-white w-full flex flex-col">
         <div className="w-full flex flex-row justify-between">
           <div className="w-1/3"></div>
           <span className="items-center h-10 mt-[20px] md:mt-[30px] text-[rgb(1,84,80)] tracking-normal font-bold text-center text-[28px] font-work-sans leading-7 md:leading-[22px]"> Páscoa Jovem 2024 </span>
           <div className="w-1/3 flex justify-center items-center pt-3 font-proxima font-extralight">
-            <Link className="w-[90px] h-[30px] md:w-[142px] md:h-[40px] bg-[#015450] flex font-xs justify-center content-center" href="https://docs.google.com/forms/d/e/1FAIpQLSetJ2tPRpLhiWGG-bjHxysMctuPs5tEzyugp5NoepFxY64nig/viewform?usp=sf_link" > Inscrições </Link>
+            <Link className="w-[90px] h-[30px] md:w-[142px] md:h-[40px] bg-[#015450] flex font-xs justify-center content-center rounded-lg" href="https://docs.google.com/forms/d/e/1FAIpQLSetJ2tPRpLhiWGG-bjHxysMctuPs5tEzyugp5NoepFxY64nig/viewform?usp=sf_link" > Inscrições </Link>
           </div>
         </div>
         <div className="mt-[25px] h-10 bg-[#f7f7f7] text-black flex justify-center">
           <div className="flex flex-row justify-between items-center font-proxima font-thin text-black">
             <Link className="px-2.5" href="/"> Home </Link>
-            <Link className="px-2.5" href="/ClickToPray"> Click to Pray </Link>
+            <Link className="px-2.5" href="/clicktopray"> Click to Pray </Link>
             <Link className="px-2.5" href="/horario"> Horário </Link>
             <Link className="px-2.5" href="/musicas"> Músicas </Link>
           </div>
