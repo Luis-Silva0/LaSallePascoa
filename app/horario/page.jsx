646d2fdeda2cdb7e390be0437082885b5b02b791
Luis Silva <biokiller134@gmail.com>
2024-03-03 15:26:16 +0000

    Production

--- app/horario/page.jsx
+++ app/horario/page.jsx
@@ -17,21 +17,7 @@ export default function Horario() {
 
     return (
         <div className="bg-white flex flex-col items-center w-full">
-            <div className="w-[65%] md:w-[70%] py-[50px]">
-                <h1 className="flex text-[#015450] text-6xl font-work-sans md:justify-normal justify-center"> Schedule </h1>
-            </div>
-            <div className="w-[80%] md:w-[65%] flex flex-col gap-8">
-                {datas.map((data,index) => (
-                    <div className="flex flex-col gap-1.5">
-                        <h2 className="text-[#015450] font-work-sans"> {dia}, {data} </h2>
-                        <div className="flex flex-col gap-3" key={index}>
-                            {atividadesPorDia(data).map((atividade,i) => (
-                                <Schedule key={i} ativ={atividade}/>
-                            ))}
-                        </div>
-                    </div>
-                ))}
-            </div>
+            Ainda não é Páscoa
         </div>
     );
 }
