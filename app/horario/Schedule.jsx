f9501b2ef8e144f0a258aa6186d8d4a8055baef4
luissilva <biokiller134@gmail.com>
2024-03-28 01:10:54 +0000

    Actual end

--- app/horario/Schedule.jsx
+++ app/horario/Schedule.jsx
@@ -1,22 +1,7 @@
-import { IoMdMusicalNote } from "react-icons/io";
-import { MdLocationPin } from "react-icons/md";
 "use client";
 import cores from "/data/cores"
 
-export default function Schedule({ativ}) {
-    console.log(ativ)
-    if (ativ) return(
-        <div className="flex flex-row text-black font-work-sans border-t-[1px] border-[#015450] justify-between pt-3">
-            <span className="w-[30%] md:max-w-[170px] px-1"> {ativ.nome} </span>
-            <div className="flex flex-row w-[64px]">
-                <IoMdMusicalNote/>
-                <MdLocationPin/>
-            </div>
-            <div className="flex flex-row justify-center px-1">
-                <span className="w-[42px] flex justify-end"> {ativ.horaInicial} </span>
-                <span className="px-1.5"> - </span>
-                <span className="w-[42px] flex justify-start"> {ativ.horaFinal} </span>    
-            </div>
+
 export default function Schedule({schedule}) {
     const date = new Date();
     const dia = date.getDay();
@@ -31,5 +16,5 @@ export default function Schedule({schedule}) {
                 </div>
             ))}
         </div>
-        );
+    );
 }
